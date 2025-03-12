53f8113eabac2dc57286adaf32338953b594d96d
ryanzinhim <ryanvictor2019@outlook.com>
2025-03-12 10:19:05 -0300

    subindo os primeiros

---
+++ 1002/program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode._1002
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            double r = 10; //double.Parse(Console.ReadLine());
+
+            double Pi = 3.14159;
+            double A = Pi * (r * r);
+            Console.WriteLine("A= ", A);
+        }
+    }
+}
